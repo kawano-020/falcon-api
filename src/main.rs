661c3093db8d615b50313c057e42121e727064fa
kawano-020 <orca.inc0510@gmail.com>
2021-12-08 21:32:24 +0900

    hello api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    format!("Hello, World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
